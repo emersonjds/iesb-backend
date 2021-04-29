8b9047b1af273a300d6d2b236cae05bdac033506
Emerson Silva <emerson_jdss@hotmail.com>
2021-04-29 20:00:59 -0300

    config post route

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 4000
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  let preco = 30;
-  let qtd = 5;
-  let total = preco * qtd;
-  console.log(`variavel ${total}`)
-  res.send('Hello World')
+    let preco = 30;
+    let qtd = 5;
+    let total = preco * qtd;
+    console.log(`variavel ${total}`)
+    res.send('Hello World')
+})
+
+app.post('/example/:productId', (req, res) => {
+    const {price, name, product} = req.body;
+    const {ord, pag} = req.query;
+    const {idProduct} = req.params;
+    const {franquia, quadra} = req.headers;
+
+    console.log(price, name, product)
+
+
+
+  if(res.status(201)) {
+    res.send({message: 'Item criado com sucesso'})
+  }
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port} !`)
+    console.log(`Example app listening on port ${port} !`)
 })
